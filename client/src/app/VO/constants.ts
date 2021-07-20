84279d3e00aa13d3e8a9f36cddf2b9dd61cb9b3f
dev-sankhadip <sankhadip.2000@gmail.com>
2021-07-20 09:36:59 +0530

    CSV bug fixed

--- client/src/app/VO/constants.ts
+++ client/src/app/VO/constants.ts
@@ -1,3 +1,3 @@
 export const TokenExceptURL = ['/menu/items', '/auth/signup', '/auth/check-user', '/admin/login']
 
-export const CSVColumns = ['Order_Id', 'Created_On', 'Order_Total', 'Delivery_Type', 'Delivery_Area', 'Address', 'Identification', 'Total_Amount'];
+export const CSVColumns = ['Order_Id', 'Created_On', 'Order_Total', 'Order_Type_Desc', 'Delivery_Area_Desc', 'Address', 'Identification', 'Total_Amount'];
